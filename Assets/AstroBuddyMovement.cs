3c34f25399231f3bc0a4feff2c97d87ddb750750
kenny <krlu91@gmail.com>
2020-05-13 23:47:06 -0400

    switched astropunk directional facing and can now jump while running

--- Assets/AstroBuddyMovement.cs
+++ Assets/AstroBuddyMovement.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 
 public class AstroBuddyMovement : MonoBehaviour{
     public CharacterController2D controller;
+    private Rigidbody2D rigidBody;
     float horizontalMove = 0f;
     bool didCrouch = false;
-    bool didJump = false;
+    bool didJump = true;
 
     public float runSpeed = 40;
   
     // Start is called before the first frame update
     void Start(){
-        
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update(){
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
-        didJump = Input.GetButtonDown("Jump");
+        didJump = Input.GetKey("space");
     }
 
     void FixedUpdate(){
         controller.Move(horizontalMove * Time.fixedDeltaTime, didCrouch, didJump);
-        didJump = false;
     }
 
 }
